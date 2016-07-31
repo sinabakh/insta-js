b59ae32244fff79b12bbc7658f35e29ffd4141fb
Sina Bakhtiari <sinabakh44@live.com>
2016-07-31 15:52:34 +0430

    Updated request to load next urls

--- src/request.ts
+++ src/request.ts
@@ -46,8 +46,50 @@ module Request {
         xhttp.send(null);
     }
 
-    export function request( url : string , callback : Function ) {
-        make_request( this.base_url + url + '?access_token=' + this.token , function(response) {
+    function get_next( pagination:any, callback : Function) {        
+        if(pagination.next_url) {
+            make_request(pagination.next_url, function(response){
+                response = JSON.parse(response);
+                if(response.meta.code == 200)
+                {
+                    let res = {
+                        data: response.data,
+                        next: response.pagination 
+                    };                
+                    callback(res);
+                } 
+                else
+                {
+                    //FIXME
+                    console.log("ERROR:");
+                    console.log(response.meta.error_message);
+                    
+                    //callback(undefined);
+                }
+            });
+        } 
+        else {
+            callback({});
+        }
+    }
+
+    export function get_next_till( times : number, pagination:any, callback : Function) {
+        times--;
+        get_next(pagination, function(res){
+            if(times == 1)
+                callback(res);
+            else
+                get_next_till(times, pagination, callback);
+        });
+    }
+
+    export function request( url : string, params:any , callback : Function ) {
+        let final_url = this.base_url + url + '?access_token=' + this.token;
+        for(var key in params)
+        {   
+            final_url += "&" + key + "=" + params[key];
+        }
+        make_request( final_url , function(response) {
                 response = JSON.parse(response);
                 if(response.meta.code == 200)
                 {
